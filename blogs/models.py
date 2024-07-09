e3bbabf98ed730bcaaf410ce2cf19cb0565e56b8
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-09 12:15:32 -0400

    agregando foto por defecto en blog

--- blogs/models.py
+++ blogs/models.py
@@ -7,7 +7,12 @@ class Blog(models.Model):
     body = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(null=True, blank=True, upload_to="blog_picture/")
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="blog_picture/",
+        default="/blog_picture/noticia.jpg",
+    )
     REQUIRED_FIELDS = ["body", "user"]
 
     # Para que se muestre en el modulo de administración
