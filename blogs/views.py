310ea00fabe6deb5d09bc31631b015971cceccea
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-07-12 20:32:25 -0400

    cambio en views.py Se le agregó un try por si no encuentra el blog, también se le agregó que envie el id del blog que se borró

--- blogs/views.py
+++ blogs/views.py
@@ -11,7 +11,7 @@ from rest_framework.schemas import ManualSchema
 
 
 @api_view(["GET"])
-# Reviso si está autentificado.
+#!: Reviso si está autentificado.
 @permission_classes([IsAuthenticated])
 def getBlogs(request):
     blog = Blog.objects.filter().order_by("-date")
@@ -20,7 +20,7 @@ def getBlogs(request):
 
 
 @api_view(["GET"])
-# Reviso si está autentificado.
+#!: Reviso si está autentificado.
 @permission_classes([IsAuthenticated])
 def getSoloBlog(request, pk):
     blog = Blog.objects.get(id=pk)
@@ -29,23 +29,23 @@ def getSoloBlog(request, pk):
 
 
 @api_view(["POST"])
-# Reviso si está autentificado y si tiene rol de Editor o Administrador.
+#!: Reviso si está autentificado y si tiene rol de Editor o Administrador.
 @permission_classes([IsAuthenticated & IsAdmin | IsAuthenticated & IsEditor])
 def postBlog(request):
     data = request.data
     image_file = request.FILES.get("image")
 
-    # Creo un diccionario con los datos que siempre se pasan.
+    # todo: Creo un diccionario con los datos que siempre se pasan.
     blog_data = {
         "user": request.user,
         "body": data["body"],
     }
 
-    # Si me pasaron alguna imagen, la añado al diccionario.
+    # todo: Si me pasaron alguna imagen, la añado al diccionario.
     if image_file is not None:
         blog_data["image"] = image_file
 
-    # Creo el objeto Blog con los datos del diccionario.
+    # todo: Creo el objeto Blog con los datos del diccionario.
     blog = Blog.objects.create(**blog_data)
 
     serializer = BlogSerializer(blog, many=False)
@@ -53,13 +53,13 @@ def postBlog(request):
 
 
 @api_view(["POST"])
-# Reviso si está autentificado.
+#!: Reviso si está autentificado.
 @permission_classes([IsAuthenticated])
 def uploadImage(request, pk):
     data = request.data
     user = data["user"]
     blog = blog.objects.get(id=pk)
-    # Reviso que el usuario que hizo el blog sea el mismo que la esté actualizando la foto o que tenga el rol de admin.
+    #!: Reviso que el usuario que hizo el blog sea el mismo que la esté actualizando la foto o que tenga el rol de admin.
     if blog.user == user or user.role == "admin":
         blog.image = request.FILES.get("image")
         blog.save()
@@ -69,13 +69,13 @@ def uploadImage(request, pk):
 
 
 @api_view(["PUT"])
-# Reviso si está autentificado.
+# !: Reviso si está autentificado.
 @permission_classes([IsAuthenticated])
 def putBlog(request, pk):
     data = request.data
     blog = Blog.objects.get(id=pk)
     serializer = BlogSerializer(instance=blog, data=data)
-    # Reviso que el usuario que hizo el blog sea el mismo que la esté actualizando o que tenga el rol de admin.
+    #!: Reviso que el usuario que hizo el blog sea el mismo que la esté actualizando o que tenga el rol de admin.
     if blog.user == request.user or request.user.role == "admin":
         if serializer.is_valid():
             serializer.save()
@@ -85,21 +85,27 @@ def putBlog(request, pk):
 
 
 @api_view(["DELETE"])
-# Reviso si está autentificado.
+#!: Reviso si está autentificado.
 @permission_classes([IsAuthenticated])
 def deleteBlog(request, pk):
-    blog = Blog.objects.get(id=pk)
+    # Busco el blog segun el id.
+    try:
+        blog = Blog.objects.get(id=pk)
+    except Blog.DoesNotExist:
+        return Response(
+            {"Error": "Publicación no encontrada"}, status=status.HTTP_404_NOT_FOUND
+        )
 
-    # Reviso que el usuario que hizo el blog sea el mismo que la esté borrando o que tenga el rol de admin.
+    #!: Reviso que el usuario que hizo el blog sea el mismo que la esté borrando o que tenga el rol de admin.
     if blog.user == request.user or request.user.role == "admin":
         blog.delete()
-        return Response("Publicación Eliminada")
+        return Response({"Mensaje": "Publicación Eliminada", "id": pk})
     else:
         return Response({"Error": "No autorizado"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(["POST"])
-# Reviso si está autentificado.
+#!: Reviso si está autentificado.
 @permission_classes([IsAuthenticated])
 def comment(request, pk):
     blog = Blog.objects.get(id=pk)
