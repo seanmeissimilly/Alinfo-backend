05fcd2730b9c04d39474728888ef8b7fd830c0a3
Sean Meissimilly <seanmeissimilly@yahoo.com>
2024-08-19 14:49:58 -0400

    Arreglando problema con la base de datos.

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "alinfo_db",
+        "NAME": "alinfo",
         "USER": "postgres",
         "PASSWORD": "Contra12345*",
         "HOST": "localhost",
