d522eafb005aec2658aaf4b3bb0481bf0fb1d22c
Sean Meissimilly <seanmeissimilly@yahoo.com>
2025-06-11 19:02:00 -0400

    bugs

--- backend/settings.py
+++ backend/settings.py
@@ -188,7 +188,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
+# DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
